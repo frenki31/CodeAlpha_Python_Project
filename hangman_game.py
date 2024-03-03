238a9236c88bd5afa4356d2d4aa2bcebd5f454cd
Frenki Muca <frenki@muca.com>
2024-03-04 00:40:14 +0100

    hangman_game updated

--- hangman_game.py
+++ hangman_game.py
@@ -6,27 +6,30 @@ request = requests.get(word_url)
 words = request.text.splitlines()
 word = random.choice(words)
 correct_guess = word
-guessed_word = ""
+guessed_word = "*" * len(word)
 count = 0
 while correct_guess != guessed_word:
-    print(word)
-    print(f'Word to guess: {"*" * len(word)}')
+    # print(word)
+    print(f'Word to guess: {guessed_word}')
     incorrect_guesses = 0
-    while incorrect_guesses < 5:
-        guess_letter = str(input(f"Guess letter {str(count + 1)}: "))
-        if guess_letter != word[count]:
-            if incorrect_guesses < 4:
-                print("Incorrect guess. Try again...")
+    while incorrect_guesses < (len(word)*2-2):
+        guess_letter = str(input("Guess a letter: "))
+        if guess_letter not in word:
+            if incorrect_guesses < (len(word)*2-3):
                 incorrect_guesses += 1
-            elif incorrect_guesses == 4:
-                print("Game over! You reached the retries limit")
+                print(f"Wrong guess. You have {(len(word)*2-2)-incorrect_guesses} incorrect guesses remaining.")
+            elif incorrect_guesses == (len(word)*2-3):
+                print("Game over! You reached the guesses limit")
                 break
         else:
-            guessed_word += guess_letter
-            incorrect_guesses = 0
+            for i, letter in enumerate(word):
+                if letter == guess_letter:
+                    index = i
+                    guessed_word = guessed_word[:index]+guess_letter+guessed_word[index+1:]
+            print(f'Guessed word until now: {guessed_word}')
             count += 1
         if guessed_word == correct_guess:
-            print("Congratulations!! You guessed it!")
+            print(f"Congratulations!! You guessed it!")
             break
         else:
             continue
